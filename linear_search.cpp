c6299fe3eab2541fa1d9bef1aef8d4315314b070
CHANDAN KUMAR <111732919+789chandan@users.noreply.github.com>
2023-08-08 08:25:53 +0530

    Add files via upload
    Enter the Size of an ARRAY:
    5
    Enter the element of an ARRAY:
    10 20 50 40 5
    10 20 50 40 5 
    Enter the key:
    5
    
     Element is present at index 4

---
+++ linear_search.cpp
@@ -0,0 +1,36 @@
+// WAP to check wheather the number is present or not using linear search
+#include<stdio.h>
+int arr[100];
+ int linear_search(int arr[],int num, int key){ // function for linear search
+	for(int i=0;i<num;i++)
+		if(arr[i] == key)
+			return i;
+		return -1 ;
+	
+}
+void print(int arr[], int num){ // function created
+	for(int i=0;i<num;i++){
+		printf("%d",arr[i]);
+		printf(" ");
+	}
+}
+int main(){
+	int num;
+	int key = 1;
+	printf("Enter the Size of an ARRAY:\n");
+	scanf("%d",&num);
+	printf("Enter the element of an ARRAY:\n");
+	for(int i=0;i<num;i++){
+		scanf("%d",&arr[i]);
+	}
+	print(arr,num); // function calling
+    
+	int result = linear_search(arr, num, key); // function call
+	
+	if(result == -1){
+		printf("\n Element is not present");
+	}else
+	    printf("\n Element is present at index %d", result);
+	
+	return 0;
+}
